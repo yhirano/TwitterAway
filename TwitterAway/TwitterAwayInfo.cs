e611b8a6aab3be1148f9cf7043e355a89231b722
hirano <devnull@localhost>
2007-05-04 10:25:29 +0000

    ・バージョン番号の変更 ・プロジェクトファイルが必要のないファイルを見ていたので、それを削除

--- TwitterAway/TwitterAwayInfo.cs
+++ TwitterAway/TwitterAwayInfo.cs
@@ -29,7 +29,7 @@ namespace TwitterAway
         /// <summary>
         /// アプリケーションのバージョン
         /// </summary>
-        private const string VERSION_NUMBER = "0.4 (Test Release 1)";
+        private const string VERSION_NUMBER = "0.4";
 
         /// <summary>
         /// アプリケーションのバージョン
